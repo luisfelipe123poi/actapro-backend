687af056950f5df7ae4777c0616a9c5d0277486b
luisfelipe123poi <luiscastellar1505@gmail.com>
2026-08-15 16:32:33 -0500

    main.py

--- main.py
+++ main.py
@@ -705,7 +705,7 @@ async def descargar_acta(acta_id: str, email: str):
     )
 
 import os
-import fitz  # PyMuPDF
+import PyMuPDF
 import pdfplumber
 from fastapi import FastAPI, File, Form, UploadFile, HTTPException
 from typing import Optional
