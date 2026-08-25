2a5cd7fe7d9d006fc946777e397526dac04e9458
luisfelipe123poi <luiscastellar1505@gmail.com>
2026-08-25 14:27:25 -0500

    celery_app.py

--- celery_app.py
+++ celery_app.py
@@ -4,13 +4,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# Lee la URL de Redis desde tus variables de entorno o usa el localhost por defecto
-REDIS_URL = os.getenv("REDIS_URL", "red-da6uafrncjis73f70rgg")
+# Si existe la variable REDIS_URL en Render la usa; si estás en local, usa localhost por defecto
+REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379/0")
 
 celery_app = Celery(
     "actapro_tasks",
     broker=REDIS_URL,
-    backend=REDIS_URL
+    backend=REDIS_URL,
+    include=["tasks"]  # Registra las tareas de tasks.py en el worker
 )
 
 celery_app.conf.update(
